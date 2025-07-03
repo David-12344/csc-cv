96cd382a5b8bde0f45528b494b9e98b4a5b7bfbe
David-12344 <kingdavidbb.1@gmail.com>
2025-07-03 14:04:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 
 face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-image = cv2.imread('img.webp')
+image = cv2.imread('proxy-image.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
